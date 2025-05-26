98a6b47fdeb9d40662434c27a43a863743fa7800
yd-tw <yd960528@gmail.com>
2025-05-26 22:15:39 +0800

    add es rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    rules: {
+      "react-hooks/exhaustive-deps": "warn",
+      "prefer-const": "warn",
+      "@typescript-eslint/no-explicit-any": "warn"
+    },
+  })
 ];
 
 export default eslintConfig;
